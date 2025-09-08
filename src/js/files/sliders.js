869ab6cb6569b2a4bf7dba8a3ae750d28dc22281
pauchok-sk <pauchoooooook@gmail.com>
2025-09-08 16:33:50 +0300

    s-step dev

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -5,13 +5,29 @@ export default function sliders() {
     const swiper = new Swiper(portfolioSlider, {
       speed: 800,
       slidesPerView: "auto",
-      // autoplay: {
-      //   delay: 3500,
-      // },
+      autoplay: {
+        delay: 3500,
+      },
       navigation: {
         prevEl: ".s-portfolio .slider-nav__btn._prev",
-        nextEl: ".s-portfolio .slider-nav__btn._next"
-      }
-    })
+        nextEl: ".s-portfolio .slider-nav__btn._next",
+      },
+    });
+  }
+
+  const reviewsSlider = document.querySelector(".s-reviews__slider");
+
+  if (reviewsSlider) {
+    const swiper = new Swiper(reviewsSlider, {
+      speed: 800,
+      spaceBetween: 15,
+      autoplay: {
+        delay: 3200,
+      },
+      navigation: {
+        prevEl: ".s-reviews .slider-arrow._prev",
+        nextEl: ".s-reviews .slider-arrow._next",
+      },
+    });
   }
 }
